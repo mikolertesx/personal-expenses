99ba375a7cb31d285703373eafad8b291d66d150
Mikolertesx <mgangsal@outlook.com>
2020-11-13 09:39:35 -0600

    Looping through lists

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      double totalSum = 0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        final currentTransaction = recentTransactions[i];
+        if (currentTransaction.date.day == weekDay.day &&
+            currentTransaction.date.month == weekDay.month &&
+            currentTransaction.date.year == weekDay.year) {
+          totalSum += currentTransaction.amount;
+        }
+      }
+
+      return {'day': DateFormat.E(weekDay), 'amount': 9.99};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
