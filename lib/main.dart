fc0229305f42929dad4301566305a9157c9e678f
Mikolertesx <mgangsal@outlook.com>
2020-11-13 09:11:24 -0600

    Custom fonts and working with text themes.

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,21 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 20,
+                ),
+              ),
+        ),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
       ),
       home: MyHomePage(),
     );
@@ -69,7 +84,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Personal expenses'),
+        title:
+            Text('Personal expenses', style: TextStyle(fontFamily: 'OpenSans')),
         actions: [
           IconButton(
             icon: Icon(Icons.add),
